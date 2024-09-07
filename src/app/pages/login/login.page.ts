f6c86dd047feb2672fc54157dfc76fd991573ebd
Davide <davide.n2@gmx.de>
2024-09-07 20:35:55 +0200

    new folder structure + login area html almost finished

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
+  password = ''; 
+  showPassword = false;
   constructor() { }
 
   ngOnInit() {
   }
+
+
+
+
+
 
 }
