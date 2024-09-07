a0f907f84334dc182b7b5faabf21affd7c5df001
Davide <davide.n2@gmx.de>
2024-09-07 18:21:50 +0200

    Update global.scss and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     './src/**/*.ts',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'darkBlue': '#2A3647',
+        'lightBlue': '#29ABE2',
+        'whiteSmoke': '#F5F5F5', 
+      },
+      boxShadow: {
+        'lightShadow': '0px 4px 6px 0px rgba(0, 0, 0, 0.33);',
+        'smallShadow': '0px 0px 14px 3px rgba(0, 0, 0, 0.04);',
+      }
+    },
+
   },
   plugins: [],
 }
