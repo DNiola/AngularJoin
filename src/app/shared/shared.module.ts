a6f2c8747bea0ca5dfe8c694063075101471ccd7
Davide <davide.n2@gmx.de>
2024-09-08 22:42:07 +0200

    Add auth-footer component to login and sign-up pages

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,9 +7,10 @@ import { AuthHeaderComponent } from '../components/auth-header/auth-header.compo
 import { RouterModule } from '@angular/router';
 import { AuthInputFieldsComponent } from '../components/auth-input-fields/auth-input-fields.component';
 import { AuthCheckboxComponent } from '../components/auth-checkbox/auth-checkbox.component';
+import { AuthFooterComponent } from '../components/auth-footer/auth-footer.component';
 @NgModule({
-  declarations: [AuthFormComponent, AuthHeaderComponent, AuthInputFieldsComponent, AuthCheckboxComponent],
+  declarations: [AuthFormComponent, AuthHeaderComponent, AuthInputFieldsComponent, AuthCheckboxComponent, AuthFooterComponent],
   imports: [CommonModule, IonicModule, FormsModule, RouterModule],
-  exports: [AuthFormComponent, AuthHeaderComponent, AuthInputFieldsComponent, AuthCheckboxComponent]
+  exports: [AuthFormComponent, AuthHeaderComponent, AuthInputFieldsComponent, AuthCheckboxComponent, AuthFooterComponent]
 })
 export class SharedModule { }
