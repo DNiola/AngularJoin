6946e5d72d6f31a6ac5292562fe1fc5018db3b68
Davide <davide.n2@gmx.de>
2024-09-11 18:38:06 +0200

    refactor summary page layout and styling

--- src/app/pages/summary/summary.module.ts
+++ src/app/pages/summary/summary.module.ts
@@ -14,7 +14,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    SummaryPageRoutingModule, SharedModule
+    SummaryPageRoutingModule, 
+    SharedModule
   ],
   declarations: [SummaryPage]
 })
