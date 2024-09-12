25bb45bf1ed0d4bd07296129b15bcbe4fb546af8
Davide <davide.n2@gmx.de>
2024-09-12 16:56:42 +0200

    header component added dropdown menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +9,39 @@ import { User } from 'src/app/models/user.model';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+
   @Input() currentUser: User | null = null;
 
+  public menuItems = [
+    { text: 'Help', href: '/help' },
+    { text: 'Legal Notice', href: '/legal-notice' },
+    { text: 'Privacy Policy', href: '/privacy-policy' }
+  ]
 
+  public isDropdownOpen = false
+  @ViewChild('dropdownMenu') dropdownMenu!: ElementRef;
+  @ViewChild('userInitials') userInitials!: ElementRef;
+
+  constructor(private userService: UserService, private router: Router) { }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent) {
+    // make sure the elements exist before accessing them
+    if (!this.dropdownMenu || !this.userInitials) {
+      return;  // exit if the elements do not exist
+    }
+
+    const clickedOutsideDropdown = !this.dropdownMenu?.nativeElement.contains(event.target); // check if clicked outside the dropdown
+    const clickedOutsideInitials = !this.userInitials?.nativeElement.contains(event.target); // check if clicked outside the initials
+
+    if (clickedOutsideDropdown && clickedOutsideInitials) {
+      this.isDropdownOpen = false; // close the dropdown if clicked outside
+    }
+  }
+
+
+  logout() {
+    this.userService.clearUser();
+    this.router.navigate(['/login']);
+  }
 }
