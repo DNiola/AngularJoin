e64a453fddc5f94be20b9f4a920251c1fc49aa58
Davide <davide.n2@gmx.de>
2024-09-13 12:15:21 +0200

    added greeting in summary page to dynamically display appropriate greeting based on current time

--- src/app/pages/summary/summary.page.ts
+++ src/app/pages/summary/summary.page.ts
@@ -10,13 +10,30 @@ import { UserService } from 'src/app/services/user.service';
 export class SummaryPage implements OnInit {
 
   public currentUser: User | null = null;
+  public greeting: string = '';
 
   constructor(private userService: UserService) { }
 
-  ngOnInit() {
+ public ngOnInit(): void {
     this.userService.currentUser$.subscribe(user => {
       this.currentUser = user;
     });
+    this.getGreetings();
+
   }
 
+
+  private getGreetings(): void {
+    const currentHour = new Date().getHours();
+
+    if (currentHour >= 22 || currentHour < 6) {
+      this.greeting = 'Good Night';
+    } else if (currentHour >= 18) {
+      this.greeting = 'Good Evening';
+    } else if (currentHour >= 12) {
+      this.greeting = 'Good Afternoon';
+    } else {
+      this.greeting = 'Good Morning';
+    }
+  }
 }
