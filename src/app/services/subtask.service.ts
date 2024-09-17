a62320117ae57dde8dc09b5317f09d5d7d79f413
Davide <davide.n2@gmx.de>
2024-09-17 13:01:08 +0200

    Added SubtaskService for managing subtasks

---
+++ src/app/services/subtask.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SubtaskService {
+  private subtasksSubject = new BehaviorSubject<string[]>([]);
+  subtasks$: Observable<string[]> = this.subtasksSubject.asObservable();
+
+  public updateSubtasks(subtasks: string[]): void {
+    this.subtasksSubject.next([...subtasks]);
+  }
+
+
+  public addSubtask(subtask: string): void {
+    const currentSubtasks = this.subtasksSubject.getValue();
+    this.subtasksSubject.next([...currentSubtasks, subtask]);
+  }
+
+
+  public deleteSubtask(index: number): void {
+    const currentSubtasks = this.subtasksSubject.getValue();
+    this.subtasksSubject.next(currentSubtasks.filter((_, i) => i !== index));
+  }
+
+
+  public updateSubtask(index: number, updatedSubtask: string): void {
+    const currentSubtasks = this.subtasksSubject.getValue();
+    currentSubtasks[index] = updatedSubtask;
+    this.subtasksSubject.next([...currentSubtasks]);
+  }
+}
