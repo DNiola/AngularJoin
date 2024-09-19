39f11f5badde0f2898802778dce15c8321930292
Davide <davide.n2@gmx.de>
2024-09-19 22:06:15 +0200

    task-card component display category colors and truncate task description

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,20 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Task } from 'src/app/models/task.model';
 @Component({
   selector: 'app-task-card',
   templateUrl: './task-card.component.html',
   styleUrls: ['./task-card.component.scss'],
 })
-export class TaskCardComponent {
+export class TaskCardComponent implements OnInit {
+
   @Input() task: Task | null = null;
+
   public cardTitle: string = '';
-  constructor() {
+  public colorTitle: string = '';
 
-
-  }
   ngOnInit() {
-    if (Array.isArray(this.task?.category) && this.task?.category.length > 0) { 
+    if (Array.isArray(this.task?.category) && this.task?.category.length > 0) {
       this.cardTitle = this.task.category[0].text;
+      this.colorTitle = this.task.category[0].color; 
     }
   }
 }
--- src/app/models/category.model.ts
+++ src/app/models/category.model.ts
@@ -1,6 +1,7 @@
 export interface Category {
   text: string;
   selected: boolean;
+  color: string;
 }
 
 export type Categorys = Category[];
