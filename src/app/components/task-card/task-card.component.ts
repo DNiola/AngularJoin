70d7dc836eced6c91d260d59f9bf84d1e4b06e25
Davide <davide.n2@gmx.de>
2024-09-20 14:47:28 +0200

    Refactor TaskCardComponent calculate percentage of completed subtasks

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,21 +1,43 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Task } from 'src/app/models/task.model';
+import { subTask, Task } from 'src/app/models/task.model';
 @Component({
   selector: 'app-task-card',
   templateUrl: './task-card.component.html',
   styleUrls: ['./task-card.component.scss'],
 })
 export class TaskCardComponent implements OnInit {
-
   @Input() task: Task | null = null;
 
-  public cardTitle: string = '';
-  public colorTitle: string = '';
+  public cardTitle = '';
+  public colorTitle = '';
 
-  ngOnInit() {
+  public doneSubtasks: subTask[] = [{ title: '', done: false }]
+  public percentDoneSubtasks = 0;
+
+
+  public ngOnInit(): void {
+    this.initCategory();
+    this.initSubtasks();
+  }
+
+
+  private initCategory(): void {
     if (Array.isArray(this.task?.category) && this.task?.category.length > 0) {
-      this.cardTitle = this.task.category[0].text;
-      this.colorTitle = this.task.category[0].color; 
+      const category = this.task?.category?.[0];
+      if (category) {
+        this.cardTitle = category.text;
+        this.colorTitle = category.color;
+      }
     }
   }
+
+
+  private initSubtasks(): void {
+    this.doneSubtasks = this.task?.subtasks?.filter(subtask => subtask.done) || [];
+    console.log(`Erledigte Subtasks: ${this.doneSubtasks.length}`);
+
+    this.percentDoneSubtasks = this.doneSubtasks.length / (this.task?.subtasks?.length || 1) * 100;
+    console.log(`Prozent erledigter Subtasks: ${this.percentDoneSubtasks}%`);
+  }
+
 }
