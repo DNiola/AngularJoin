bac593741e58d37d442b4cd2c7eb059d457ff980
Davide <davide.n2@gmx.de>
2024-09-25 19:41:27 +0200

    Added ContactService for saving contacts to Firestore

---
+++ src/app/services/contact.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Contact } from '../models/contact.model';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+  constructor(private firestore: AngularFirestore) { }
+
+  public saveContact(creatorId: string, newContact: Contact): Promise<void> {
+    const userRef = this.firestore.collection('users').doc(creatorId);
+  
+    return this.firestore.firestore.runTransaction(async (transaction) => {
+      const userDoc = await transaction.get(userRef.ref);
+  
+      if (!userDoc.exists) {
+        throw new Error('Benutzer existiert nicht');
+      }
+  
+      const userData = userDoc.data() as any;
+      const updatedContacts = userData.contacts ? [...userData.contacts, newContact] : [newContact];
+  
+      transaction.update(userRef.ref, { contacts: updatedContacts });
+    });
+  }
+}
