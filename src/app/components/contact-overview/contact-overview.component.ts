554821627947337c01f88e6e5932b0a5d7b225d8
Davide <davide.n2@gmx.de>
2024-09-26 14:16:25 +0200

    contact-overview component handle contact deletion and editing

--- src/app/components/contact-overview/contact-overview.component.ts
+++ src/app/components/contact-overview/contact-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Contact } from 'src/app/models/contact.model';
 
 @Component({
@@ -6,11 +6,18 @@ import { Contact } from 'src/app/models/contact.model';
   templateUrl: './contact-overview.component.html',
   styleUrls: ['./contact-overview.component.scss'],
 })
-export class ContactOverviewComponent  implements OnInit {
+export class ContactOverviewComponent {
   @Input() selectedContact: Contact | null = null;
 
-  constructor() { }
+  @Output() handleContact = new EventEmitter<{ action: 'delete' | 'edit'; contact: Contact }>();
 
-  ngOnInit() {}
+
+  public onDeleteContact(contact: Contact): void {
+    this.handleContact.emit({ action: 'delete', contact });
+  }
+
+  public onEditContact(contact: Contact): void {
+    this.handleContact.emit({ action: 'edit', contact });
+  }
 
 }
