19f5b31ad96a93fb23424e9304f3679c12fc0374
Davide <davide.n2@gmx.de>
2024-09-26 14:16:06 +0200

    contact service include methods for deleting and hiding contacts

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -11,18 +11,55 @@ export class ContactService {
 
   public saveContact(creatorId: string, newContact: Contact): Promise<void> {
     const userRef = this.firestore.collection('users').doc(creatorId);
-  
+
     return this.firestore.firestore.runTransaction(async (transaction) => {
       const userDoc = await transaction.get(userRef.ref);
-  
+
       if (!userDoc.exists) {
         throw new Error('Benutzer existiert nicht');
       }
-  
+
       const userData = userDoc.data() as any;
       const updatedContacts = userData.contacts ? [...userData.contacts, newContact] : [newContact];
-  
+
       transaction.update(userRef.ref, { contacts: updatedContacts });
     });
   }
+
+
+  public deleteUserContact(currentUserId: string, contactId: string): Promise<void> {
+    const userRef = this.firestore.collection('users').doc(currentUserId);
+
+    return this.firestore.firestore.runTransaction(async (transaction) => {
+      const userDoc = await transaction.get(userRef.ref);
+
+      if (!userDoc.exists) {
+        throw new Error('Benutzer existiert nicht.');
+      }
+
+      const userData = userDoc.data() as any;
+      const updatedContacts = userData.contacts.filter((c: Contact) => c.userId !== contactId);
+
+      transaction.update(userRef.ref, { contacts: updatedContacts });
+    });
+  }
+
+
+  public hideUserForCurrentUser(currentUserId: string, contact: Contact): Promise<void> {
+    const userRef = this.firestore.collection('users').doc(currentUserId);
+
+    return this.firestore.firestore.runTransaction(async (transaction) => {
+      const userDoc = await transaction.get(userRef.ref);
+
+      if (!userDoc.exists) {
+        throw new Error('Benutzer existiert nicht.');
+      }
+
+      const userData = userDoc.data() as any;
+      const updatedHidden = userData.hidden ? [...userData.hidden, contact.userId] : [contact.userId];
+
+      transaction.update(userRef.ref, { hidden: updatedHidden });
+    });
+  }
+
 }
