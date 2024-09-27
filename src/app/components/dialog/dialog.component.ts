8a33611a9029a997ed85054ed5bbeff53e8ee6ad
Davide <davide.n2@gmx.de>
2024-09-27 18:09:37 +0200

    added dialog component (not finish)

---
+++ src/app/components/dialog/dialog.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss'],
+})
+export class DialogComponent {
+  @Input() public dialogMessage = { title: '', message: '', showAgain: false, isSingUpSection: false };
+
+  @Output() confirm = new EventEmitter<void>();
+  @Output() cancel = new EventEmitter<void>();
+
+
+  public onConfirm(): void {
+    debugger
+    if (this.dialogMessage.showAgain == true) {
+      localStorage.setItem('disclaimerUnderstood', 'true');
+    }
+    this.confirm.emit();
+  }
+
+
+  public onCancel(): void {
+    this.cancel.emit();
+  }
+
+
+  public onCheckboxChange(checked: boolean): void {
+    this.dialogMessage.showAgain = checked;
+  }
+}
