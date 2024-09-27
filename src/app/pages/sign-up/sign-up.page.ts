0afdb39553d50842fb35e07b58de52b76f013fa6
Davide <davide.n2@gmx.de>
2024-09-27 18:09:47 +0200

    Added dialog component to sign-up page

--- src/app/pages/sign-up/sign-up.page.ts
+++ src/app/pages/sign-up/sign-up.page.ts
@@ -6,13 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.page.scss'],
 })
 export class SignUpPage implements OnInit {
+  public isDialogOpen = true ;
+
+  public dialogMessage = {
+    title: 'Attention !',
+    message: 'Please do not use real credentials to log in to this site. This site was created for educational and training purposes and the security of the data cannot be fully guaranteed. Please also note that all data stored on this site can only be deleted manually by the administrators. For more information, please see our legal notice.',
+    showAgain: false,
+    isSingUpSection: true
+  };
 
   constructor() { }
 
   ngOnInit() {
+    const disclaimerUnderstood = localStorage.getItem('disclaimerUnderstood');
+    if (disclaimerUnderstood) {
+      this.isDialogOpen = localStorage.getItem('disclaimerUnderstood') !== 'true';
+    }
   }
-
-
-
-  
 }
