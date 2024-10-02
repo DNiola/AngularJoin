a42c3ee10508c05bb88886054b6242c4c9a49fae
Davide <davide.n2@gmx.de>
2024-10-02 19:46:28 +0200

    sidebar component need current user for display information

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
+
+
 export class SidebarComponent {
+  public currentUser: User | null = null;
 
+  constructor(private userService: UserService) { }
 
+  public ngOnInit(): void {
+     this.userService.currentUser$.subscribe((user) => {
+      this.currentUser = user;
+    });
+  }
 
 }
