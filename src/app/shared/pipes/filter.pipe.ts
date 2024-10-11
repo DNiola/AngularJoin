feb5b46c41bfa4a4993d9661c0d5a6911d5c4873
Davide <davide.n2@gmx.de>
2024-10-11 15:41:48 +0200

    FilterPipe include JSDoc

--- src/app/shared/pipes/filter.pipe.ts
+++ src/app/shared/pipes/filter.pipe.ts
@@ -5,7 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(items: any[], searchTerm: string, keys: string[]): any[] {
+  /**
+   * Filters an array of items based on a search term and specified keys.
+   *
+   * This method takes an array of objects and filters it by checking if the search term is included
+   * in any of the specified keys of each object. It performs a case-insensitive search.
+   *
+   * @param {any[]} items - The array of items to be filtered.
+   * @param {string} searchTerm - The term to search for in the item properties.
+   * @param {string[]} keys - The list of keys in the items to be used for matching the search term.
+   * @returns {any[]} The filtered array of items that match the search term in the specified keys.
+   */
+  public transform(items: any[], searchTerm: string, keys: string[]): any[] {
     if (!items || !searchTerm) {
       return items;
     }
