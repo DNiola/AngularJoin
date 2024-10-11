cacd770c044e5f09394fa175c8c43f6d7bcda337
Davide <davide.n2@gmx.de>
2024-10-11 15:47:32 +0200

    auth guard add JSDoc

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -4,6 +4,16 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 import { map } from 'rxjs/operators';
 
+/**
+ * Auth guard to protect routes from unauthorized access.
+ *
+ * This guard checks if the user is authenticated before allowing access to the route.
+ * If the user is not authenticated, they are redirected to the login page.
+ *
+ * @param {ActivatedRouteSnapshot} route - The route to be activated.
+ * @param {RouterStateSnapshot} state - The state of the router at the time of activation.
+ * @returns {Observable<boolean>} Observable that emits true if the user is authenticated, otherwise false.
+ */
 export const authGuard: CanActivateFn = (route, state) => {
   const afAuth = inject(AngularFireAuth);
   const router = inject(Router);
@@ -11,10 +21,10 @@ export const authGuard: CanActivateFn = (route, state) => {
   return afAuth.authState.pipe(
     map(user => {
       if (user) {
-        return true;  // User is logged in, allow access
+        return true;
       } else {
-        router.navigate(['/login']);  // Not logged in, redirect to login page
-        return false;  // Deny access
+        router.navigate(['/login']);
+        return false;
       }
     })
   );
