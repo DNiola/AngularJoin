50b1d382eefb8a064e3d908a88b98862e899309c
Davide <davide.n2@gmx.de>
2024-10-13 13:11:38 +0200

    Adjust user bubble size and dialog in header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -18,22 +18,35 @@ export class HeaderComponent {
     { text: 'Help', link: 'info' },
     { text: 'Legal Notice', link: 'legal' },
     { text: 'Privacy Policy', link: 'policy' }
-  ]
+  ];
 
-  public isDropdownOpen = false
+  public isDropdownOpen = false;
   public isDialog = false;
+  public isAnimation = false;
 
   constructor(private userService: UserService, private router: Router) { }
 
+  
+  /**
+   * Initializes the header component by subscribing to the current user data.
+   * 
+   * @returns {void}
+   */
   public ngOnInit(): void {
-     this.userService.currentUser$.subscribe((user) => {
+    this.userService.currentUser$.subscribe((user) => {
       this.currentUser = user;
     });
   }
 
-  
+
+  /**
+   * Handles click events outside of the dropdown menu and user initials to close the dropdown.
+   * 
+   * @param {MouseEvent} event - The mouse event triggered by clicking on the document.
+   * @returns {void}
+   */
   @HostListener('document:click', ['$event'])
-  handleClickOutside(event: MouseEvent): void {
+  public handleClickOutside(event: MouseEvent): void {
     if (!this.dropdownMenu || !this.userInitials) {
       return;
     }
@@ -47,9 +60,18 @@ export class HeaderComponent {
   }
 
 
-  logout(): void {
+  /**
+   * Logs out the current user, shows a logout animation, and redirects to the login page.
+   * 
+   * @returns {void}
+   */
+  public onLogout(): void {
     this.isDialog = false;
+    this.isAnimation = true;
     this.userService.clearUser();
-    this.router.navigate(['/login']);
+    setTimeout(() => {
+      this.isAnimation = false;
+      this.router.navigate(['/login']);
+    }, 1000);
   }
 }
