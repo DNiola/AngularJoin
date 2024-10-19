536a91d49c8b93fd7f9cbc4bb0a36580d1b01037
Davide <davide.n2@gmx.de>
2024-10-19 14:20:38 +0200

    Refactor sidebar button component to use isActive property for active state

--- src/app/components/sidebar-button/sidebar-button.component.ts
+++ src/app/components/sidebar-button/sidebar-button.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar-button',
@@ -7,18 +8,31 @@ import { Router } from '@angular/router';
   styleUrls: ['./sidebar-button.component.scss'],
 })
 export class SidebarButtonComponent implements OnInit {
-  @Input() text = ''
-  @Input() icon = ''
-  @Input() link = ''
+  @Input() text = '';
+  @Input() icon = '';
+  @Input() link = '';
 
-  public active = false
+  public isActive: boolean = false;
+  private routerSubscription: Subscription | undefined;
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    if (this.router.url === this.link && !this.active) {
-      this.active = true
-      return
+    this.updateActiveState();
+    this.routerSubscription = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {      
+      this.updateActiveState();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
     }
+  }
+
+  private updateActiveState(): void {
+    this.isActive = this.router.url.startsWith(this.link);
   }
 }
